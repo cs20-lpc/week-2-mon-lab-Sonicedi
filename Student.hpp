c1e89234ed089ab80d27f4997e62c78890a86682
Edsson Ibanez <edssonibanez@Edssons-MacBook-Air.local>
2025-08-27 19:50:42 -0700

    Completed lab assignment

---
+++ Student.hpp
@@ -0,0 +1,27 @@
+#ifndef STUDENT_HPP
+#define STUDENT_HPP
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+    private:
+        string* name;
+        int* age;
+
+    public:
+
+    Student(); //constructor
+
+    ~Student(); //destructor
+
+    void setName(string);
+    void setAge(int);
+
+    string getName() const;
+    int getAge()const;
+    
+};
+
+#endif
